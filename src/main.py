3065bb5e82cec7b92cd6b380f8d7f77c906f38b3
Davidmachadocb <davidmachadocbec@gmail.com>
2025-11-20 17:12:47 -0300

    chore: initial src folder and main.py file

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from optic.comm.modulation import modulateGray, demodulateGray, grayMapping
+from optic.dsp.core import pnorm, upsample, firFilter, pulseShape, signal_power, phaseNoise
+from optic.models.devices import mzm, iqm
+from optic.utils import parameters, dBm2W
+from optic.plot import eyediagram
